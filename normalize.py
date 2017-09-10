bca1d1de756eb11d5fa2fe379fe355f375ee93ac
Seth Frost <dovekie@gmail.com>
2017-09-09 21:51:06 -0700

    all standardization functions work

--- normalize.py
+++ normalize.py
@@ -23,13 +23,19 @@ class Row():
         self.total_duration = None
         self.notes = notes
 
-    def utf_timestamp(self):
+    def iso_timestamp(self):
+        """Converts a string to a ISO-8601 timestamp
+        returns: string
+        """
         pacific_time = self.timestamp + ' PST'
         time = datetime.strptime(pacific_time, '%m/%d/%y %I:%M:%S %p %Z')
         eastern_time = self.convert_to_eastern(time)
         return eastern_time.isoformat()
 
     def pad_zip_code(self):
+        """Left-pad a zip code with zeroes
+        Returns: string
+        """
         padded_zip = self.zip_code
         if len(padded_zip) < 5:
             padding = 5 - len(padded_zip)
@@ -37,8 +43,35 @@ class Row():
         return padded_zip
 
     def convert_to_eastern(self, time):
+        """Convert a datetime to US Eastern
+        Args: time (datetime) a datetime object in PST
+        """
         offset = timedelta(hours=3)
         return time + offset
+
+    def create_timedelta(self, duration):
+        """
+        """
+        delta = [float(time) for time in duration.split(':')]
+        return timedelta(hours=delta[0], minutes=delta[1], seconds=delta[2])
+
+    def calc_total_duration(self):
+        foo_delta = self.create_timedelta(self.foo_duration)
+        bar_delta = self.create_timedelta(self.bar_duration)
+
+        return foo_delta + bar_delta
+
+    def create_DTO(self):
+        DTO = []
+        DTO.append(self.iso_timestamp())
+        DTO.append(self.address)
+        DTO.append(self.pad_zip_code())
+        DTO.append(self.full_name)
+        DTO.append(self.create_timedelta(self.foo_duration).total_seconds())
+        DTO.append(self.create_timedelta(self.bar_duration).total_seconds())
+        DTO.append(self.calc_total_duration())
+        DTO.append(self.notes)
+        return DTO
 
 def remove_header(reader):
     for row in reader:
@@ -47,9 +80,7 @@ def remove_header(reader):
     return headers
 
 def create_row_objects(reader):
-    remove_header(reader)
     all_rows = []
-
     for row in reader:
         new_row = Row(row[0],
                       row[1],
@@ -58,11 +89,19 @@ def create_row_objects(reader):
                       row[4],
                       row[5],
                       row[7])
-        new_row.utf_timestamp()
+        new_row.iso_timestamp()
         all_rows.append(new_row)
     return all_rows
 
-
 reader = csv.reader(sys.stdin)
-for row in create_row_objects(reader):
-    print(row.address)
+headers = remove_header(reader)
+all_rows = create_row_objects(reader)
+
+with open('normalized.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, 
+                        delimiter=',', 
+                        quotechar='"', 
+                        quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(headers)
+    for row in all_rows:
+        writer.writerow(row.create_DTO())
